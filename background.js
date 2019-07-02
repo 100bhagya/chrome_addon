7a7f3c53ce826c5efd6333bc871494eed333733f
Saubhagya <saubhagya@dailyobjects.com>
2019-07-02 15:06:42 +0530

    * used browser(chrome) properties * added background.js * updated main.js * updated mainfest

--- background.js
+++ background.js
@@ -7,4 +7,5 @@ chrome.tabs.onUpdated.addListener(
         })
       }
     }
-  );
+  ); 
+  
--- main.js
+++ main.js
@@ -38,3 +38,4 @@ chrome.runtime.onMessage.addListener(
                     generate_deeplink();
             }
     });
+    
